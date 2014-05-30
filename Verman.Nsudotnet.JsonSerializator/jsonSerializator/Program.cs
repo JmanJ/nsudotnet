718794737b30632e7efeabf56c65259bfe9864b3
JmanJ <jmanj@mail.ru>
2014-05-30 19:32:13 +0600

    ...
    ...

---
+++ Verman.Nsudotnet.JsonSerializator/jsonSerializator/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace JsonSerializator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var obj = new TestObject();
+            var serializator = new Serializator(obj);
+            serializator.Serialize();
+            Console.Write(serializator.GetResult());
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Verman.Nsudotnet.JsonSerializator/jsonSerializator/TestObject.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace JsonSerializator
+{
+    [Serializable]
+    class TestObject
+    {
+        public int i;
+        public string s;
+
+        [NonSerialized]
+        public string ignore; // это поле не должно сериализоваться
+
+        public string[] arrayMember;
+        public bool b;
+
+        public TestObject()
+        {
+            i = 5;
+            s = "Hahaha";
+            ignore = "IgnoreMe";
+            arrayMember = new string[] { "1", "3", "5", "7", "9" };
+            b = true;
+        }
+    }
+}
